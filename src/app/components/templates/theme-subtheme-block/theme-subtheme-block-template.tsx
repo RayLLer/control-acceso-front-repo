99653bce8641242eca41db84d4a3c7d411abcf23
davidleond99 <84214167+davidleond99@users.noreply.github.com>
2024-08-16 15:51:46 -0400

    fix delete subtheme

--- src/app/components/templates/theme-subtheme-block/theme-subtheme-block-template.tsx
+++ src/app/components/templates/theme-subtheme-block/theme-subtheme-block-template.tsx
@@ -1,21 +1,21 @@
-'use client'
-import { Tabs } from 'antd';
-import React from 'react';
-import ThemeTable from './components/theme-table';
-import SubThemeTable from './components/sub-theme-table';
-import BlockTable from './components/blocks-table';
+"use client";
+import { Tabs } from "antd";
+import React from "react";
+import ThemeTable from "./components/theme-table";
+import SubThemeTable from "./components/sub-theme-table";
+import BlockTable from "./components/blocks-table";
 
 const ThemeSubthemeBlockTemplate = () => {
-  const [selectedIndex, setSelectedIndex] = React.useState('theme')
+  const [selectedIndex, setSelectedIndex] = React.useState("theme");
   return (
-    <Tabs defaultActiveKey='theme' onChange={setSelectedIndex}>
-      <Tabs.TabPane tab='Temas' key='theme'>
+    <Tabs defaultActiveKey="theme" onChange={setSelectedIndex}>
+      <Tabs.TabPane tab="Temas" key="theme">
         <ThemeTable />
       </Tabs.TabPane>
-      <Tabs.TabPane tab='Subtemas' key='sub-theme'>
+      <Tabs.TabPane tab="Subtemas" key="sub-theme">
         <SubThemeTable />
       </Tabs.TabPane>
-      <Tabs.TabPane tab='Bloques' key='block'>
+      <Tabs.TabPane tab="Bloques" key="block">
         <BlockTable />
       </Tabs.TabPane>
     </Tabs>
