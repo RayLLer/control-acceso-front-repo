453ccb3c0341b93e2cce435f7bbc8182b7f2835d
davidleond99 <84214167+davidleond99@users.noreply.github.com>
2024-08-16 13:13:39 -0400

    Add Cancel Button

--- src/app/components/templates/reset-password/reset-password-template.tsx
+++ src/app/components/templates/reset-password/reset-password-template.tsx
@@ -1,9 +1,9 @@
-'use client';
-import { paths } from '@/app/routes/paths';
-import { axiosBaseInstance } from '@/utils/axios';
-import { Flex, Card, Form, Input, Button, App } from 'antd';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+"use client";
+import { paths } from "@/app/routes/paths";
+import { axiosBaseInstance } from "@/utils/axios";
+import { Flex, Card, Form, Input, Button, App } from "antd";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const ResetPasswordTemplate = () => {
   const router = useRouter();
@@ -14,25 +14,25 @@ const ResetPasswordTemplate = () => {
     try {
       setLoading(true);
       const response = await axiosBaseInstance.post(
-        'auth/reset-password',
+        "auth/reset-password",
         values,
         {
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         }
       );
       setLoading(false);
       notification.success({
-        message: 'Contraseña cambiada',
-        description: 'Su contraseña ha sido cambiada con éxito',
+        message: "Contraseña cambiada",
+        description: "Su contraseña ha sido cambiada con éxito",
       });
       router.push(paths.login);
     } catch (error) {
       setLoading(false);
       notification.error({
-        message: 'Error',
-        description: 'Hubo un error al cambiar la contraseña',
+        message: "Error",
+        description: "Hubo un error al cambiar la contraseña",
       });
     }
   };
@@ -40,93 +40,93 @@ const ResetPasswordTemplate = () => {
   return (
     <div
       style={{
-        flexDirection: 'column',
-        width: '100vw',
-        height: '100vh',
+        flexDirection: "column",
+        width: "100vw",
+        height: "100vh",
         margin: 0,
         padding: 0,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundImage: 'url(/img/login-background.png)',
-        backgroundSize: 'cover',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundImage: "url(/img/login-background.png)",
+        backgroundSize: "cover",
       }}
     >
-      <Card
-        title='Cambiar la contraseña'
-        style={{ width: 500 }}
-      >
+      <Card title="Cambiar la contraseña" style={{ width: 500 }}>
         <p>
           Copie el código del correo que se le envió y péguelo aquí y cambie su
           contraseña.
         </p>
         <Form
-          name='forgotForm'
-          layout='horizontal'
+          name="forgotForm"
+          layout="horizontal"
           onFinish={onFinish}
           labelWrap
           labelCol={{ span: 6 }}
           wrapperCol={{ span: 16 }}
         >
           <Form.Item
-            label='Código'
-            name='code'
+            label="Código"
+            name="code"
             rules={[
               {
                 required: true,
                 message:
-                  'Por favor ingrese el código del correo que se le envió',
+                  "Por favor ingrese el código del correo que se le envió",
               },
             ]}
           >
-            <Input placeholder='zertyoaizndoianzodianzdonaizdoinaozdnia' />
+            <Input placeholder="zertyoaizndoianzodianzdonaizdoinaozdnia" />
           </Form.Item>
 
           <Form.Item
-            name='password'
-            label='Contraseña'
+            name="password"
+            label="Contraseña"
             rules={[
               {
                 required: true,
-                message: 'Introduzca la contraseña',
+                message: "Introduzca la contraseña",
               },
               {
                 min: 6,
-                message: 'La contraseña debe tener al menos 6 caracteres',
+                message: "La contraseña debe tener al menos 6 caracteres",
               },
             ]}
             hasFeedback
           >
-            <Input.Password placeholder={'••••••••'} />
+            <Input.Password placeholder={"••••••••"} />
           </Form.Item>
 
           <Form.Item
-            name='passwordConfirmation'
-            label='Confirmar Contraseña'
-            dependencies={['password']}
+            name="passwordConfirmation"
+            label="Confirmar Contraseña"
+            dependencies={["password"]}
             hasFeedback
             rules={[
               {
                 required: true,
-                message: 'Confirme su contraseña',
+                message: "Confirme su contraseña",
               },
               ({ getFieldValue }) => ({
                 validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
+                  if (!value || getFieldValue("password") === value) {
                     return Promise.resolve();
                   }
                   return Promise.reject(
-                    new Error('Las contraseñas no coinciden')
+                    new Error("Las contraseñas no coinciden")
                   );
                 },
               }),
             ]}
           >
-            <Input.Password placeholder={'••••••••'} />
+            <Input.Password placeholder={"••••••••"} />
           </Form.Item>
           <Form.Item>
-            <Button type='primary' htmlType='submit' loading={loading}>
+            <Button type="primary" htmlType="submit" loading={loading}>
               Enviar
+            </Button>
+            <Button type="link" onClick={() => router.push(paths.login)}>
+              Cancelar
             </Button>
           </Form.Item>
         </Form>
