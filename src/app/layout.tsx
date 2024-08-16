d6065810a0ca55d88b59191582809b709d7991c9
davidleond99 <84214167+davidleond99@users.noreply.github.com>
2024-08-16 13:01:29 -0400

    Fix x scroll margin 0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,22 @@
-import type { Metadata } from 'next';
-// import { Inter } from 'next/font/google';
-import React from 'react';
-import './globals.css';
-import Providers from './providers/provider';
-
-// const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from "next";
+import React from "react";
+import "./globals.css";
+import Providers from "./providers/provider";
 
 export const metadata: Metadata = {
-  title: 'Test Opo',
-  description: 'El giro que necesitas',
+  title: "Test Opo",
+  description: "El giro que necesitas",
 };
-
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
-    <html lang='en'>
-      <body style={{margin: 0 }} >
-          <Providers>{children}</Providers>
+    <html lang="en">
+      <body style={{ marginTop: 0, marginLeft: 0 }}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
