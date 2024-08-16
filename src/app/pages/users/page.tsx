69ac44063a9d1f979f687b9276f98a742e9f1d49
davidleond99 <84214167+davidleond99@users.noreply.github.com>
2024-08-16 17:03:11 -0400

    Update page.tsx

--- src/app/pages/users/page.tsx
+++ src/app/pages/users/page.tsx
@@ -1,59 +1,57 @@
-'use client';
-import { FC, ReactElement } from 'react';
+"use client";
+import { FC, ReactElement } from "react";
 
-
-import MagicTable from '@/app/components/table-v2/table-custom';
-import { ColumnsType } from '@/app/interfaces/strapi';
-import { useRouter } from 'next/navigation';
-import { IUser } from './users.interface';
-
+import MagicTable from "@/app/components/table-v2/table-custom";
+import { ColumnsType } from "@/app/interfaces/strapi";
+import { useRouter } from "next/navigation";
+import { IUser } from "./users.interface";
 
 const User: FC = (): ReactElement => {
-  const router = useRouter()
+  const router = useRouter();
 
   const columns: ColumnsType<IUser>[] = [
     {
-      title: 'Usuario',
-      dataIndex: ['username'],
-      key: 'username',
+      title: "Usuario",
+      dataIndex: ["username"],
+      key: "username",
       filtrable: true,
-      filterType: 'string',
+      filterType: "string",
     },
     {
-      title: 'Correo',
-      dataIndex: ['email'],
-      key: 'email',
+      title: "Correo",
+      dataIndex: ["email"],
+      key: "email",
       filtrable: true,
-      filterType: 'string',
+      filterType: "string",
     },
     {
-      title: 'Nombre Completo',
-      dataIndex: ['name'],
-      key: 'name',
+      title: "Nombre Completo",
+      dataIndex: ["name"],
+      key: "name",
       filtrable: true,
-      filterType: 'string',
+      filterType: "string",
     },
     {
-      title: 'Rol',
-      dataIndex: ['role', 'name'],
-      key: 'role.name',
+      title: "Rol",
+      dataIndex: ["role", "name"],
+      key: "role.name",
       filtrable: true,
-      filterType: 'string',
+      filterType: "string",
     },
     {
-      title: 'Bloqueado',
-      dataIndex: ['blocked'],
-      key: 'blocked',
-      render: (user: IUser) => (user.blocked ? 'Bloqueado' : 'No bloqueado'),
+      title: "Bloqueado",
+      dataIndex: ["blocked"],
+      key: "blocked",
+      render: (blocked: boolean) => (blocked ? "Bloqueado" : "No Bloqueado"),
     },
   ];
 
   return (
     <MagicTable<IUser, IUser>
       columns={columns}
-      url={'users'}
-      onAdd={() => router.push('users/form')}
-      onEdit={(id) => router.push(`users/form/${id}`,)}
+      url={"users"}
+      onAdd={() => router.push("users/form")}
+      onEdit={(id) => router.push(`users/form/${id}`)}
       deleteEntry
       crud
     />
