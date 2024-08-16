0256561147124d288405680fb6fe8cbd90be3e90
davidleond99 <84214167+davidleond99@users.noreply.github.com>
2024-08-16 14:16:26 -0400

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,7 @@
-'use client';
-import { AUTH, paths } from '@/app/routes/paths';
-import { axiosInstance } from '@/utils/axios';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
+"use client";
+import { AUTH, paths } from "@/app/routes/paths";
+import { axiosInstance } from "@/utils/axios";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import {
   Button,
   Card,
@@ -11,13 +11,13 @@ import {
   Input,
   notification,
   Typography,
-} from 'antd';
-import { isAxiosError } from 'axios';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
-import styles from './page.module.css';
-import secureStorage from 'react-secure-storage'
+} from "antd";
+import { isAxiosError } from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import secureStorage from "react-secure-storage";
 
 interface ILogin {
   identifier: string;
@@ -40,108 +40,114 @@ const Login: React.FC = () => {
     try {
       setLoading(true);
       const response = await axiosInstance.post(
-        process.env.NEXT_PUBLIC_API_URL + '/' + AUTH,
+        process.env.NEXT_PUBLIC_API_URL + "/" + AUTH,
         payload
       );
+
       const user = response.data.user;
-      secureStorage.setItem('user', JSON.stringify(user));
-      secureStorage.setItem('token', response.data.jwt);
-      secureStorage.setItem('keepSign', keepSign);
-      if(keepSign){
-        secureStorage.setItem('identifier', data.identifier);
-        secureStorage.setItem('password', data.password);
+      secureStorage.setItem("user", JSON.stringify(user));
+      secureStorage.setItem("token", response.data.jwt);
+      secureStorage.setItem("keepSign", keepSign);
+      if (keepSign) {
+        secureStorage.setItem("identifier", data.identifier);
+        secureStorage.setItem("password", data.password);
       } else {
-        secureStorage.removeItem('identifier');
-        secureStorage.removeItem('password');
+        secureStorage.removeItem("identifier");
+        secureStorage.removeItem("password");
       }
 
       // const responseFcm = await userService.putUser(user.id, user);
       setLoading(false);
       router.push(paths.tests.root);
     } catch (error: any) {
+      console.log("aki");
       setLoading(false);
       if (isAxiosError(error)) {
         notification.error({
-          message: 'Identificador o Contraseña incorrectos',
+          message: "Identificador o Contraseña incorrectos",
         });
       }
     }
   };
 
   useEffect(() => {
-    const keepSign = secureStorage.getItem('keepSign') as boolean;
+    const keepSign = secureStorage.getItem("keepSign") as boolean;
     setKeepSign(keepSign);
-    if(keepSign){
-      const identifier = secureStorage.getItem('identifier') as string;
-      const password = secureStorage.getItem('password') as string;
-      if(identifier && password){
-        form.setFieldsValue({identifier, password, keepSign })
+    if (keepSign) {
+      const identifier = secureStorage.getItem("identifier") as string;
+      const password = secureStorage.getItem("password") as string;
+      if (identifier && password) {
+        form.setFieldsValue({ identifier, password, keepSign });
       }
     }
-  }, [])
-  
+  }, []);
 
   return (
     // <Row justify='center' align='middle' style={{ minHeight: '98vh' }}>
     <div
       style={{
-        flexDirection: 'column',
-        width: '100vw',
-        height: '100vh',
+        flexDirection: "column",
+        width: "100vw",
+        height: "100vh",
         margin: 0,
         padding: 0,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundImage: 'url(/img/login-background.png)',
-        backgroundSize: 'cover',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundImage: "url(/img/login-background.png)",
+        backgroundSize: "cover",
       }}
     >
       <Image
-        src='/img/logo.png'
+        src="/img/logo.png"
         width={200}
-        alt='Logo'
-        style={{ marginBottom: '5px' }}
+        alt="Logo"
+        style={{ marginBottom: "5px" }}
       />
       <Card className={styles.card}>
         <Typography.Title level={3}>Ingresar</Typography.Title>
         <Form
-          name='normal_login'
-          className='login-form'
+          name="normal_login"
+          className="login-form"
           initialValues={{ remember: true }}
           onFinish={onLogin}
           form={form}
         >
           <Form.Item
-            name='identifier'
-            rules={[{ required: true, message: 'Introduzca el Identificador' }]}
+            name="identifier"
+            rules={[{ required: true, message: "Introduzca el Identificador" }]}
           >
             <Input
-              prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder='Identificador'
+              prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Identificador"
             />
           </Form.Item>
           <Form.Item
-            name='password'
-            rules={[{ required: true, message: 'Introduzca la Contraseña' }]}
+            name="password"
+            rules={[{ required: true, message: "Introduzca la Contraseña" }]}
           >
             <Input.Password
-              prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type='password'
-              placeholder='Contraseña'
+              prefix={<LockOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Contraseña"
             />
           </Form.Item>
 
-          <Form.Item name={'keepSign'}>
+          <Form.Item name={"keepSign"}>
             {/* <Checkbox >Recuérdame</Checkbox> */}
-            <Checkbox checked={keepSign} onChange={(value)=> setKeepSign(value.target.checked)}>Recuérdame</Checkbox>
+            <Checkbox
+              checked={keepSign}
+              onChange={(value) => setKeepSign(value.target.checked)}
+            >
+              Recuérdame
+            </Checkbox>
           </Form.Item>
 
           <Form.Item>
             <Button
-              type='primary'
-              htmlType='submit'
-              className='login-form-button'
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
               loading={loading}
             >
               Acceder
