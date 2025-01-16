849d265c92c150cbf52419b0fc12235ed6fdb0ed
nperez <nperez@countigo.cu>
2025-01-16 14:02:24 -0500

    feat: add sorting capability to finish date column in realized tests

--- src/app/components/templates/realized-tests/realized-tests-columns.tsx
+++ src/app/components/templates/realized-tests/realized-tests-columns.tsx
@@ -50,6 +50,7 @@ export const realized_tests_columns: ColumnsType<IRealizedTestAttemptsResponse>[
     {
       title: "fecha del último intento",
       dataIndex: ["finishDate"],
+      sorter: true,
       filtrable: true,
       filterType: "date",
       render: (value) =>
